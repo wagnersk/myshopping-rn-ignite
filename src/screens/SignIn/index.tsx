d444a3d154dd1153a96399501d9a843a5151a752
wagnersk <wagnereletroskateet@gmail.com>
2022-02-15 00:03:11 -0300

    Módulo firebase concluído

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,{useState} from 'react';
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
@@ -6,6 +8,59 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
 export function SignIn() {
+  const [email,setEmail] = useState('')
+  const [password,setPassword]=useState('')
+  
+
+async function handleSignInAnonymously(){
+  const { user} = await auth().signInAnonymously()
+}
+
+function handleCreateUserAccount(){
+  auth()
+  .createUserWithEmailAndPassword(email,password)
+  .then(()=>Alert.alert('Usuário criado com sucesso!'))
+  .catch(error=>{
+    console.log(error.code)
+
+    if(error.code==='auth/email-already-in-use'){
+      Alert.alert('Email não disponível. Escolha outro email para cadastrar')
+    }
+
+    if(error.code==='auth/invalid-email'){
+      Alert.alert('Email inválido!')
+    }
+
+    if(error.code==='auth/weak-passoword'){
+      Alert.alert('A senha deve ter no mínimo 6 dígitos')
+    }
+
+  })
+}
+
+ function handleSignInWithEmailAndPassword(){
+   auth()
+   .signInWithEmailAndPassword(email,password)
+   .then(({user})=>{
+     console.log(user)
+
+   })
+   .catch(error=>{
+
+    if(error.code ==='auth/user-not-found'||error.code ==='auth/wrong-password'){
+      Alert.alert('Usuário não encontrado. Email e/ou senha inválida!')
+    }
+
+   })
+ 
+} 
+
+function handleForgotPassword(){
+  auth()
+  .sendPasswordResetEmail(email)
+  .then(()=>Alert.alert('Enviamos um link no seu e-mail para você redefinir sua senha'))
+}
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +69,21 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
+
       />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleForgotPassword} />
+        <ButtonText title="Criar minha conta" onPress={handleCreateUserAccount} />
       </Account>
     </Container>
   );
--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import storage from '@react-native-firebase/storage'
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
 
 import { Container, Content, Progress, Transferred } from './styles';
+import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
+
+
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,24 +32,43 @@ export function Upload() {
     }
   };
 
+  async function handleUpload(){
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/images/${fileName}.png`);
+
+    const uploadTask = reference.putFile(image);
+    uploadTask.on('state_changed',taskSnapshop=>{
+      const percent = ((taskSnapshop.bytesTransferred/taskSnapshop.totalBytes)*100).toFixed(0)
+      setProgress(percent);
+
+      setBytesTransferred(`${taskSnapshop.bytesTransferred} transferido de ${taskSnapshop.totalBytes}`)
+    })
+
+    uploadTask.then(async()=>{
+      const imageUrl = await reference.getDownloadURL()
+      console.log(imageUrl)
+      Alert.alert('Upload Concluído com sucesso!')
+    })
+  }
+
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={() => { }}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
